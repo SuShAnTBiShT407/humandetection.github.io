38e3e66072c942e05cbbbc6812741d4be785cf19
SuShAnTBiShT407 <76996837+SuShAnTBiShT407@users.noreply.github.com>
2022-03-14 16:45:01 +0530

    update

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def video_feed():
 
 
 if __name__=='__main__':    
-    app.run(host='0.0.0.0',port='5000', debug=True)
+    app.run(host='https://humandetectionbypsb.herokuapp.com/',port='5000', debug=True)
